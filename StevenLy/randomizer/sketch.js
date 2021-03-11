76d33a7f7b5c35e8158bdd59f97fc00baec4fcc4
stevebutu <stevenly657@gmai>
2021-03-11 13:47:26 -0800

    randomizer

---
+++ StevenLy/randomizer/sketch.js
@@ -0,0 +1,16 @@
+// pandas gang
+let pandas = [];
+
+function setup () {
+    createCanvas(600, 600);
+    background(200);
+
+    pandas[0] = "steven";
+    pandas[1] = "roundest";
+    console.log(pandas);
+
+}
+
+function draw(){
+
+}
