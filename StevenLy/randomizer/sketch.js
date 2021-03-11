dac1e06025325b27de9ba145eb52da4f7e45dc03
stevebutu <stevenly657@gmai>
2021-03-11 14:32:59 -0800

    2.1

--- StevenLy/randomizer/sketch.js
+++ StevenLy/randomizer/sketch.js
@@ -1,16 +1,57 @@
-// pandas gang
-let pandas = [];
+// pandas gang (arrays)
+let pandas = [{
+  name: "justin",
+  color: "roundest"
+}, {
+  name: "david",
+  color: "blue"
+}, {
+  name: "steven",
+  color: "lightblue"
+}, {
+  name: "abdal",
+  color: "red"
+}, {
+  name: "colonge",
+  color: "color"
+}, {
+  name: "jae",
+  color: "black"
+}];
 
-function setup () {
-    createCanvas(600, 600);
-    background(200);
+let randomIndex;
 
-    pandas[0] = "steven";
-    pandas[1] = "roundest";
-    console.log(pandas);
+function setup() {
+  createCanvas(600, 600);
+  background(200);
+
+  // *notes for myself*
+  // console.log(pandas[2].color);
+  // console.log("initial array is ");
+  // console.log(pandas);
+
+  // pandas.shift();
+  // console.log("array after shift ");
+  // console.log(pandas);
+
+  // pandas.splice (4, 1), x = the one you want to remove in arrays, y = how many you want in the parameter to remove
+
+  // pandas.unshift("ha");
+  // console.log("array after unshift ");
+  // console.log(pandas);
 
 }
 
-function draw(){
+function draw() {
 
+}
+
+function mousePressed() {
+    background(random(200, 255));
+  // random index = pulling a random integer to the length of the array
+    randomIndex = int(random(pandas.length));
+  // using text to sub integer for the name of panda
+    text(pandas[randomIndex].name, 50, 50);
+  // not letting it to repeat
+    pandas.splice(randomIndex, 1);
 }
