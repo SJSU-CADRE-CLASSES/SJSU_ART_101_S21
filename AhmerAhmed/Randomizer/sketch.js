b49ed80fc8e480f1f3f886987d4d4e55fc255bf4
ahmerahm18 <70781541+ahmerahm18@users.noreply.github.com>
2021-03-16 14:12:52 -0700

    sketch.js

--- AhmerAhmed/Randomizer/sketch.js
+++ AhmerAhmed/Randomizer/sketch.js
@@ -22,13 +22,14 @@ function setup() {
   pink - 255, 89, 225
   blue - 145, 255, 251
   */
-  
+
   createCanvas(400, 400);
   background(0);
 
+  fill(255)
+  text("click to change the artists and the songs", 50, 300)
   for(let i=0; i<7; i++){
-    let rando;
-    rando = int(random(5));
+    let rando = int(random(5));
     console.log("artist is " + songs[rando].name, ", song name is " + songs[rando].song);
     fill(255, 200, 156);
     text("artist: ", 10, 50+i*15);
@@ -42,4 +43,23 @@ function setup() {
 
 function draw() {
 
+}
+
+function mousePressed(){
+  background(0);
+
+  fill(255)
+  text("click to change the artists and the songs", 50, 300)
+
+  for(let i=0; i<7; i++){
+    let rando = int(random(5));
+    console.log("artist is " + songs[rando].name, ", song name is " + songs[rando].song);
+    fill(255, 200, 156);
+    text("artist: ", 10, 50+i*15);
+    text("song: ", 160, 50+i*15);
+    fill(255, 89, 225);
+    text(songs[rando].name, 50, 50+i*15);
+    fill(145, 255, 251);
+    text(songs[rando].song, 200, 50+i*15);
+  }
 }
