ce721a3456f07a0af179c2e456a3dcdb3671a4f2
julesaur <33582881+julesaur@users.noreply.github.com>
2021-03-16 16:08:19 -0700

    random haha

--- Julee Nhek/Randomizer/sketch.js
+++ Julee Nhek/Randomizer/sketch.js
@@ -1,18 +1,40 @@
 //wish you were here with me
 
-let owl = [];
+let owl = [{
+    name: "owlena",
+    color: "pink"
+    }, {
+    name: "seer",
+    color: "violet"
+    }, {
+    name: "hammy",
+    color: "cream"
+    }, {
+    name: "waves",
+    color: "turquoise"
+    }, {
+    name: "moon",
+    color: "opal"
+    }, {
+    name: "gem",
+    color: "sparkle"
+    }];
 
-  function setup() {
-  createCanvas(600, 600);
-  background(220);
-
-  owl[1] = "owlena eyes";
-
-  console.log("initial array is" + owl);
+    let randomIndex;
 
 
-}
+    function setup() {
+      createCanvas(600, 600);
+      background(220);
+    }
 
-function draw() {
+    function draw(){
 
-}
+    }
+
+    function mousePressed() {
+      background(random(212, 180, 209))
+      randomIndex = int(random(owl.length));
+      text(owl[randomIndex].name, 50, 50);
+      owl.splice(randomIndex, 1);
+    }
