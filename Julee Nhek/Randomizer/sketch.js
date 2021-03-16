08d1ac88628e8076105ec9f9ecc3e1d86ded8dd8
julesaur <33582881+julesaur@users.noreply.github.com>
2021-03-16 14:01:44 -0700

    hello index

---
+++ Julee Nhek/Randomizer/sketch.js
@@ -0,0 +1,18 @@
+//wish you were here with me
+
+let owl = [];
+
+  function setup() {
+  createCanvas(600, 600);
+  background(220);
+
+  owl[1] = "owlena eyes";
+
+  console.log("initial array is" + owl);
+
+
+}
+
+function draw() {
+
+}
