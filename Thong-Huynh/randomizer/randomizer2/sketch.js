62b95e08a03810c0191e0cb9fb326b2d9e5120ac
Thong-Huynh <minhthong.huynh@sjsu.edu>
2021-03-18 23:31:28 -0700

    update randomizer 2

--- Thong-Huynh/randomizer/randomizer2/sketch.js
+++ Thong-Huynh/randomizer/randomizer2/sketch.js
@@ -20,6 +20,10 @@ let catchMe = [{
 
 let randomIndex1;
 let randomIndex2;
+let animating = false;
+let run;
+
+
 
 function setup() {
   createCanvas(400, 400);
@@ -27,19 +31,41 @@ function setup() {
   translate(width*0.5, width*0.5);
   textSize(16);
   textStyle(BOLD);
-  text("Try to catch me IF YOU CAN!",width*-0.25,0);
+  text("Try to catch me IF YOU CAN!",width*-0.3,0);
 
+  run = new Run(width * 0.1, height * 0.5,50);
 
 }
 
 function draw() {
-  
+  if (animating == true){
+    background(random(200,255));
+
+    run.display();
+    run.move();
+
+  }
+}
+
+
+
+function randomizer(){
+  animating = false;
+  if (catchMe[0]) {
+    background(random(200,255));
+    randomIndex1 = int(random(catchMe.length));
+    randomIndex2 = int(random(catchMe.length));
+    text(`${catchMe[randomIndex1].name}
+${catchMe[randomIndex2].color}`, random(width*0, width*0.6), random(width*0.1, width*0.9));
+    catchMe.splice(randomIndex1,1);
+  } else {
+    background(random(200,255));
+    text("Uugh...U...caught me...", width*0.25, width*0.5);
+  }
 }
 
 function mousePressed() {
-  background(random(200,255));
-  randomIndex1 = int(random(catchMe.length));
-  randomIndex2 = int(random(catchMe.length));
-  text(catchMe[randomIndex1].name +
-       catchMe[randomIndex2].color, random(width*0, width*0.6), random(width*0.1, width*0.9));
+  animating = true;
+  setTimeout(randomizer, 1500);
+  // 1000 = 1s
 }
