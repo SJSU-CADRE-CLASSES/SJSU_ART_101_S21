5236b24780587e20a82902f04d5a50defbd80be4
mcordonero <mcordonero@gmail.com>
2021-03-22 02:29:12 -0700

    randomizer

---
+++ Cordonero/Randomizer/sketch.js
@@ -0,0 +1,14 @@
+// Plants that i currently own
+let plants = ["Ogre's Ear", "Lucky bamboo", "Mickey Mouse Cactus", "Hoya Heart", "Ficus Ginseng", "Money Tree", "Coconut"];
+
+function setup() {
+  createCanvas(600,600);
+  background(200);
+
+console.log (plants[0])
+
+}
+
+function draw() {
+
+}
