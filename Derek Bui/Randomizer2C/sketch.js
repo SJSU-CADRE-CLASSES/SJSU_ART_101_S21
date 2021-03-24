7c18466adeacc2b73847baadc8b69b0d6d425ba3
Dairyrice <derek.bui@sjsu.edu>
2021-03-23 20:54:00 -0700

    more pics

--- Derek Bui/Randomizer2C/sketch.js
+++ Derek Bui/Randomizer2C/sketch.js
@@ -1,12 +1,14 @@
 
-let dogs = [{name: "sippi", color: "red"}
-, {name:"duke", color: "blue"},
-{name: "stark", color: "green"},
-{name: "Boomy", color: "white"}];
+let dogs = [{name: "William", color: "red"}
+, {name:"Joe", color: "blue"},
+{name: "Andy", color: "green"},
+{name: "Eddie", color: "white"}];
 
 let randomIndex;
 let animating = false;
 let pic = [];
+let imageCounter = 1;
+let button;
 
 function preload(){
 
@@ -20,15 +22,26 @@ function setup() {
   createCanvas(400, 400);
     background(255, 400, 30);
     textSize(30);
+    imageMode(CENTER);
+    frameRate(0.5);
 
     text("click to randomize", 50, 50);
+
+    button = createButton("click to randomize");
+    button.mousePressed(buttonPressed);
 }
 
 
 function draw() {
 
   if(animating == true){
-  image(pic[1], 0, 0);
+    clear();
+  image(pic[imageCounter], width/2, height/2);
+  if (imageCounter < pic.length){
+    imageCounter++;
+  } else{
+    imageCounter = 1;
+  }
   }
 }
 
@@ -48,7 +61,7 @@ animating = false;
   }
 }
 
-function mousePressed(){
+function buttonPressed(){
 animating = true;
 setTimeout(randomizer, 2000);
 
