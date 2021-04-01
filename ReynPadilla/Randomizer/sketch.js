6bf37ca037167adce6b58cfeb5fbad2621a03c05
rendilla <reynlouise.padilla@sjsu.edu>
2021-04-01 16:28:11 -0700

    Update sketch.js

--- ReynPadilla/Randomizer/sketch.js
+++ ReynPadilla/Randomizer/sketch.js
@@ -1,3 +1,8 @@
+//types of houseplants
+let plants = ["anthurium", "philodendron", "calatheas", "pothos", "peperomia" ];
+
+console.log(plants[0]);
+
 function setup() {
   createCanvas(400, 400);
 }
