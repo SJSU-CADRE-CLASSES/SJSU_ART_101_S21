c0b6693375dcca9d56ab3c6fdf33e3a94082a4e7
huytran <huytc81@gmail.com>
2021-04-03 00:27:31 -0700

    Updated_sketch5

--- Huy_Tran/randomizer/2.4/sketch5.js
+++ Huy_Tran/randomizer/2.4/sketch5.js
@@ -1,35 +1,39 @@
 // TODAY I WANT TO EAT CHOCOLATE
 //update 040321
 
+//------------------------< START DEFINE VARIABLES >-----------------------//
 let randomIndex;
 let animating = false;
 
-let imageCounter = 0;
-let onions = [];
+let imageCounter = 0; // Beginning img
+let onions = []; //Empty variable for loading imgs from lib.
 
 let startRandomizerButton;
 let addMoredButton;
 
-let firstTime = true;
+let firstTime = true; //Adding variable for eliminating the loop.
 
-let cnv;
+let cnv; //Define variable canvas.
 
 let nameInputs = [];
 
-let onion = [];
+let onion = []; //Empty variable for adding user's input.
+//----------------------------------$$$-------------------------------------//
 
 
+///-------------------------< START LOADNG IMGS >-------------------------///
 function preload() {
   for (let i = 0; i <= 15; i++) {
     onions[i] = loadImage(`assets/onion_${i}.JPG`);
   }
 }
+//----------------------------------***-------------------------------------//
 
 
+//--------------------------START SETUP--------------------------//
 function setup() {
-
   cnv = createCanvas(350, 450);
-  cnv.parent("#canvasDiv");
+  cnv.parent("#canvasDiv"); //Assign class to define in CSS
 
   background(200);
   frameRate(8);
@@ -49,18 +53,19 @@ function setup() {
 
   // button.class("randomizerButton");
 
-for (let i = 0; i < 3; i++) {
-  nameInputs.push(createInput());
-  nameInputs[nameInputs.length - 1].parent("#inputFields");
+  for (let i = 0; i < 3; i++) {
+    nameInputs.push(createInput());
+    nameInputs[nameInputs.length - 1].parent("#inputFields");
   }
 }
+//----------------------------------***-------------------------------------//
 
 
+//--------------------------START DRAW LOOP--------------------------//
 function draw() {
   if (animating == true) {
-   clear();
-    image(onions[imageCounter], width/ 2, height/ 2);
-    image(random(onions), width/ 2, height/ 2);
+    image(onions[imageCounter], width / 2, height / 2);
+    image(random(onions), width / 2, height / 2);
 
     if (imageCounter < onions.length - 1) {
       imageCounter++;
@@ -68,25 +73,27 @@ function draw() {
     } else {
       imageCounter = 0;
     }
-
-
   }
 }
+//--------------------------------*< | >*-----------------------------------//
 
+
+//-------------------------DEFINE FUNCTION #BUTTON-------------------------//
 function addAnotherInput() {
   nameInputs.push(createInput());
   nameInputs[nameInputs.length - 1].parent("#inputFields");
 }
+//--------------------------------*OO | OO*-----------------------------------//
 
+//------------------------DEFINE FUNCTION RANDOMIZER------------------------//
 function randomizer() {
   animating = false;
- clear();
+  clear();
   if (onion[0]) {
-    // background(random(55), random(155), random(255));
-    randomIndex = int(random(onion.length));
+    randomIndex = int(random(onions.length));
 
-    image(random(onion), width/ 2, height/ 2);
-    text(onion[randomIndex], width/ 2, height - 55);
+    image(random(onions), width / 2, height / 2);
+    text(onions[randomIndex], width / 2, height - 55);
     onion.splice(randomIndex, 1);
   } else {
     background('#ff61e5');
@@ -94,7 +101,9 @@ function randomizer() {
     textAlign(CENTER);
   }
 }
+//-------------------------------*<> X <>*----------------------------------//
 
+//-----------------------DEFINE FUNCTION BUTTON STATE-----------------------//
 function buttonPressed() {
   if (firstTime) {
     for (let i = 0; i < nameInputs.length; i++) {
@@ -107,3 +116,4 @@ function buttonPressed() {
   animating = true;
 
 }
+//-------------------------------<HOHOHO>----------------------------------//
