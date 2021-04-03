f8aadf9dc51c13256b905bee68f4ae957d477fc6
huytran <huytc81@gmail.com>
2021-04-03 02:41:55 -0700

    updating 3.0

--- Huy_Tran/randomizer/3.0/sketch.js
+++ Huy_Tran/randomizer/3.0/sketch.js
@@ -1,35 +1,23 @@
-// DEFINE STATE
-let circleX = 20; //Set the initial X-position for circle
-let circleY = 20; // Set the initial Y-position for circle
-let xSpeed = 1;
-let ySpeed = 1;
 
 function setup() {
-  createCanvas(500, 300);
-  background(255, 100, 0);
-  noStroke();
-  blendMode(DIFFERENCE);
+  createCanvas(640, 480);
+  background(230);
+
+  strokeWeight(7);
 }
 
 function draw() {
-
-  // CURRENT STATE
-  circle(random(circleX* 5), circleY* 3, 10);
-  circle(circleX, random(circleY* .2), 25);
-
-  // ADJUSTING STATE
-  circleX = circleX + xSpeed;
-  circleY = circleY + ySpeed;
-
-  //BOUNCE FROM LEFT - RIGHT FROM THE ORIGIN
-  if(circleX < 0 || circleX > width) {
-    xSpeed = xSpeed * -1;
-    background(random(255), random(155), random(55));
+  if (mouseIsPressed) {
+    stroke(map(mouseX, 0, 600, 0, 255, true));
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY,);
+    line(mouseX, mouseY, pmouseX, pmouseY);
   }
+}
 
-  // BOUNCE FROM TOP - BOTTOM 
-  if(circleY < 0 || circleY > height) {
-    // ySpeed = ySpeed * -1;
-    background(random(55), random(155), random(255));
+function keyTyped() {
+  //Save canvas to file name
+  if (key === 's') {
+    saveCanvas('drawSomething', 'png');
+    return false;
   }
 }
