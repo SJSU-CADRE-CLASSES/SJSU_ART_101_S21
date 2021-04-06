0ebec558b4fd79a5441868169e9ab4653834d3a2
sjsutrantran <79670100+sjsutrantran@users.noreply.github.com>
2021-04-06 11:08:25 -0700

    2.3

--- Tran_Tran/Randomizer/randomizer2.3/randomizer.js
+++ Tran_Tran/Randomizer/randomizer2.3/randomizer.js
@@ -2,18 +2,25 @@
 let animals = [{
   name: "Spix Macaws",
   why: "Deforestation of habitat leaving only 100 alive, in captivity"
+  // img.src = 'species_0.png';
+  // <src="./assets/species_0.png">
+  // imageUrl: "https://imgur.com/a/3CYj3Wn"
 }, {
   name: "Scimatar Oryxs",
   why: "Are in rapid decline due to poaching, for it's long antlers"
+  // imageUrl:'assets/species_1.png';
 }, {
   name: "Rockhopper Penguins",
   why: "Pollution and commercial fishing are killing these species"
+  // imageUrl:'assets/species_2.png';
 }, {
   name: "Snow Leopards",
   why: "Dying due to huntings and retalitated-killings"
+  // imageUrl:'assets/species_3.png';
 }, {
   name: "Black Rhinos",
   why: "Are being poached for its horn and its medicinal properties"
+  // loadImage:'assets/species_4.png';
 }]
 
 var colorArray = [
@@ -23,11 +30,28 @@ var colorArray = [
   '#D50707'
 ]
 
+// var img = new Image();
+// img.src[0] = 'species_0.png'
+// img.src[1] = 'species_1.png'
+// img.src[2] = 'species_2.png'
+// img.src[3] = 'species_3.png'
+// img.src[4] = 'species_4.png'
+
 let randomIndex;
 
 animating = false;
 
 let button;
+
+let species = [];
+
+let imageCounter = 0;
+
+function preload(){
+  for (let i = 0; i <= 4; i++){
+  species[i] = loadImage(`assets/species_${i}.png`);
+}
+}
 
 function setup() {
 
@@ -43,6 +67,7 @@ button = createButton("Click to Kill");
 button.mousePressed(buttonPressed)
 button.style("padding", "20px");
 button.style("background-color", "#a60000");
+
 
 
 // console.log(animals.length)
@@ -63,14 +88,23 @@ button.style("background-color", "#a60000");
 function draw() {
 if (animating == true){
 stroke(random(5, 50))
-// strokeWeight(10,50);
+strokeWeight(10,50);
 noStroke();
 frameRate(20);
 fill(random(colorArray));
 ellipse(random(width), random(height),random(80, 300));
+// clear();
+// image(species[imageCounter], width/2, height/2);
+//
+// if(imageCounter < species.length - 1){
+// imageCounter++;
+// } else {
+//   imageCounter = 0;
+}
 // color(random(#FFF9F7, #FFB69E, #FF5922));
+
 }
-}
+
 
 
 function changeBackground(){
@@ -108,19 +142,22 @@ function randomizer(){
   animating = false;
   if (animals[0]){
     //displays random name and splices it out of array
-    background(random(200, 255));
+    background(random(10, 25));
     randomIndex = int(random(animals.length));
 
     fill(10);
     stroke(255);
-    strokeWeight(3);
+    strokeWeight(2);
     textSize(40);
-
+    image(species[randomIndex], 0, 0);
+    console.log(randomIndex);
     text(animals[randomIndex].name, 400, 660);
+    console.log(randomIndex);
 
     textSize(20);
     text(animals[randomIndex].why, 400, 700);
     animals.splice(randomIndex, 1);
+    species.splice(randomIndex, 1);
   } else{
     background(10);
     strokeWeight(3);
@@ -129,5 +166,14 @@ function randomizer(){
     fill(255, 0, 0);
     textSize(50);
     text("All are going extinct in 2021", 400, height/2);
+
+    if (animals.length == 0){
+      animating = false;
+    }
   }
-}
+
+  // function theEnd(){
+  //   // if (animals.length == 0){
+  //   //   animating = false;
+  //   }
+  }
