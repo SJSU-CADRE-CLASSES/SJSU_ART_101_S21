6417251bbdf152b6cb9ceda12b2c0b60f17219d8
Thong-Huynh <minhthong.huynh@sjsu.edu>
2021-04-06 00:39:46 -0700

    add Project 2

--- Thong-Huynh/randomizer/randomizer4/sketch.js
+++ Thong-Huynh/randomizer/randomizer4/sketch.js
@@ -1,47 +1,28 @@
-// let catchMe = [{
-//   name: "Oh, Come on! ",
-//   color: "I'm here. Bleh..bleh.."
-// }, {
-//   name: "Too bad... ",
-//   color: "I'm over here!"
-// }, {
-//   name: "Oops! ",
-//   color: "Still cannot catch me?"
-// }, {
-//   name: "Hehehe... ",
-//   color: "About to give up?"
-// }, {
-//   name: "Too slow... ",
-//   color: "Next time, baby!"
-// }, {
-//   name: "Faster! ",
-//   color: "Try one more time!"
-// }];
-
-let catchMe = [];
+let scene = [];
 
 let randomIndex1;
-let randomIndex2;
+
 let animating = false;
-// let run;
-let chase = [];
+
+let cop = [];
 let imageCounter = 0;
 let x;
 let y;
-let caught;
+let surrender;
 let startRandomizerButton;
 let addMoreButton;
 let cnv;
 let nameInputs = [];
 let firstTime = true;
+let t;
 
 function preload(){
 
-  for (let i = 0; i <= 7; i++){
+  for (let i = 0; i <= 5; i++){
     // trolls[i] = loadImage("assets/chasing (" + i + ").jpg"); (the same as under)
-    chase[i] = loadImage(`assets/chasing (${i}).png`);
+    cop[i] = loadImage(`assets/cop (${i}).png`);
   }
-  caught = loadImage(`assets/chasing (8).png`);
+  surrender = loadImage(`assets/cop (6).png`);
 }
 
 
@@ -50,12 +31,18 @@ function setup() {
   cnv.parent("#canvasDiv");
   // background(220);
   background(random(200,255),random(200,255),random(200,255));
-  translate(width*0.5, width*0.5);
-  textSize(24);
+  translate(width*0.5, height*0.5);
+  textSize(18);
   textStyle(BOLD);
-  text("Try to catch me IF YOU CAN!",width*-0.275,0);
+  text(`You are a well-known DETECTIVE,
+      currently investigating a SERIAL KILLER case
+      in which a suspect is hiding his LAST HOSTAGE.
 
-  // button = createButton("click to chase");
+Your MISSION is to find out where he keeps the hostage
+      by using your INTERROGATION SKILL.`,width*-0.45,height*-0.4);
+  image(cop[0], width*-0.45, height*-0.2);
+
+  // button = createButton("click to interrogate");
   startRandomizerButton = select("#randButton");
   startRandomizerButton.mousePressed(buttonPressed);
   // button.class("randomizerButton");
@@ -69,24 +56,20 @@ function setup() {
   }
 
   imageMode(CENTER);
-  frameRate(12);
+  frameRate(18);
 
-  // run = new Run(width * 0.1, height * 0.5,50);
 
 }
 
 function draw() {
   if (animating == true){
-    // background(random(200,255));
-    // run.display();
-    // run.move();
 
     clear();
     background(random(200,255));
-    // background(random(0),random(0),random(150));
-    image(chase[imageCounter], random(width), random(height));
 
-    if (imageCounter < chase.length - 1){
+    image(cop[imageCounter], random(width), random(height));
+
+    if (imageCounter < cop.length - 1){
     imageCounter++;
   } else {
     imageCounter = 0;
@@ -102,22 +85,46 @@ function addAnotherInput(){
 
 function randomizer(){
   animating = false;
-  if (catchMe[0]) {
+  if (scene[0]) {
     clear();
     background(random(200,255),random(200,255),random(200,255));
-    randomIndex1 = int(random(catchMe.length));
-    randomIndex2 = int(random(catchMe.length));
+    randomIndex1 = int(random(scene.length));
 
-    x = random(width*0.25, width*0.65);
-    y = random(height*0.05, height*0.5);
-    text(`${catchMe[randomIndex1]}
-${catchMe[randomIndex2]}`, x, y);
-    image(random(chase), x + width*0.125, y + height*0.2);
-    catchMe.splice(randomIndex1,1);
+
+    x = width*0.2;
+    y = height*0.2;
+
+    t = random(cop);
+    image(t, x + width*0.3, y + height*0.4);
+    if (t == cop[1]){
+    text(`You SMASHED the suspect's HANDS with a
+                                ${scene[randomIndex1]}`, x, y);
+  } else if (t == cop[2]){
+    text(`You CRUSHED the suspect's FEET with a
+                                ${scene[randomIndex1]}`, x, y);
+  } else if (t == cop[3]){
+    text(`You PIERCED the suspect's EARS with a
+                                ${scene[randomIndex1]}`, x, y);
+  } else if (t == cop[4]){
+    text(`You PUNCHED the suspect's FACE with a
+                                ${scene[randomIndex1]}`, x, y);
+  } else if (t == cop[5]){
+    text(`You ELECTRIFIED the suspect with a
+                                ${scene[randomIndex1]}`, x, y);
+  } else if (t == cop[0]){
+    text(`You deal NO DAMAGE to the suspect with a
+                                ${scene[randomIndex1]}`, x, y);
+  }
+
+    scene.splice(randomIndex1,1);
   } else {
     background(random(200,255),random(200,255),random(200,255));
-    text("Uugh...U...caught me...", width*0.3, height*0.35);
-    image(caught, width*0.5, height*0.5);
+    text(`Thanks to your MASTER INTERROGATION SKILL,
+      the suspect SCREAMS in pain and BEGS for forgiveness;
+      at the same time, you got HINTS to rescue the hostage.
+
+Congratulations!`, width*0.05, height*0.1);
+    image(surrender, width*0.5, height*0.6);
   }
 }
 
@@ -125,7 +132,7 @@ function buttonPressed() {
 
 if (firstTime) {
   for (let i = 0; i < nameInputs.length; i++){
-    catchMe.push(nameInputs[i].value());
+    scene.push(nameInputs[i].value());
   }
   firstTime = false;
 }
