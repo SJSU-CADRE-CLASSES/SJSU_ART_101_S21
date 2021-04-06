fffa4244ce111eba40d2f7bea42b41c3177e6240
gerardohernandez20 <42582681+gerardohernandez20@users.noreply.github.com>
2021-04-06 12:07:31 -0700

    nice

--- GerardoHz/Randomizer/sketch.js
+++ GerardoHz/Randomizer/sketch.js
@@ -1,54 +1,103 @@
- //Drifter cars
 let carros = [];
-let cars = [{name: "supra", color:"black"},{name:"nissan 240", color: "blue"},{name: "foxbody", color: "white"},{name: "lexus", color: "olive"}, {name: "miata", color: "orange"}];
+let cars = [{name: "Nice home", color:"very cold place"},{name:"yikes!", color: "good luck living there."},{name: "pretty cool", color: "hope you packed lunch."},{name: "hmmm", color: "I dont know if that habitable."}, {name: "I would live in a space shuttle", color: "If I were you"}];
 
 let randomIndex;
 let animating = false;
 let img;
 let imageCounter = 0;
+let button;
+
+let cnv;
+let nameInputs = [];
+
 
 function preload (){
+  for (let i = 0; i <= 6; i++) {
+    carros[i] = loadImage(`pictures/car${i}.jpg`)
 
-for (let i = 0; i <= 6; i++) {
-  carros[i] = loadImage(`pictures/car${i}.jpg`)
 }
-
 }
 
 
 function setup () {
-createCanvas (600, 600);
+cnv = createCanvas (800, 800);
+
+cnv.parent("#canvasDiv");
+
 background(200);
-textSize (32);
-text("click to randomize",50,50);
+textSize (28);
+imageMode(CENTER);
+frameRate (9);
+text("Click to find your new home",50,50);
+//button = createButton("Find your new home")
+
+button = select("#Button")
+button.mousePressed(buttonPressed);
+
+addmorebutton = select("#Button")
+addmorebutton.mousePressed(addAnotherInput);
+
+
+
+
+for(let i = 0; i <3; i++ ){
+nameInputs.push(createInput());
+nameInputs[nameInputs.length -1].parent("#inputFields");
+}
 console.log(carros);
 }
 function draw () {
 
+
 if (animating == true){
+  clear();
 
-image(car[0],0,0);
+image(carros[imageCounter],width/2,height/2);
+
+if (imageCounter < carros.length - 1){
+imageCounter++;
+} else {
+  imageCounter = 0;
+}
 }
 
+
 }
-function randomizer(){if (cars[0]){
+
+function addAnotherInput(){
+  nameInputs.push(createInput());
+  nameInputs[nameInputs.length -1].parent("#inputFields");
+
+}
+
+
+function randomizer(){
   animating = false;
-background(random(200, 255));
+  if (cars[0]){
+    clear();
+//background(random(200, 255));
+
+
 
 randomIndex = int(random(cars.length));
 
-text(`${cars[randomIndex].name} 's looks better in
-${cars[randomIndex].color}`, 100, 100);
 
+
+image(random(carros),width/2,height/2);
+
+text (`${cars[randomIndex].name}
+${cars[randomIndex].color}`, width/2, height - 100,);
+fill(255,255,255);
 cars.splice(randomIndex, 1);
 }else {
 background(random(200, 255))
-text("nothing left", 200,200);
+
+text("no more homes left", 200,200);
 }
 
 }
 
-function mousePressed() {
+function buttonPressed() {
   animating = true;
   setTimeout(randomizer, 2000);
 
