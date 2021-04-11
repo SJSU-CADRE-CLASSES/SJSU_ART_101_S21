aaa2bd5a1037619be37c9ab3c4862480c8ed11a4
stevebutu <stevenly657@gmai>
2021-04-10 18:49:24 -0700

    3.0

---
+++ StevenLy/drawmachine/3.0/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(600, 600);
+  background(0, 255, 255);
+  strokeWeight(5);
+}
+
+function draw() {
+
+if (mouseIsPressed){
+  stroke(map(mouseX, 0, 600, 0, 255, true))
+  line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+  line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+
+}
