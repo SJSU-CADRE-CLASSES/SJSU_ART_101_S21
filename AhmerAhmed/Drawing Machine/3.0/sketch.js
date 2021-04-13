98469bcd392faad798d1054351be4e03cc41b209
ahmerahm18 <70781541+ahmerahm18@users.noreply.github.com>
2021-04-13 13:50:01 -0700

    AhmerAhmed

--- AhmerAhmed/Drawing Machine/3.0/sketch.js
+++ AhmerAhmed/Drawing Machine/3.0/sketch.js
@@ -1,7 +1,34 @@
+//A prompt is given to the user to draw something. An image is included, but only shown for a few seconds then the end drawing is all up to the user. No restrictions to the drawing, can be literal or figurative.
+//click a key to use an eraser
+  //eraser is just stroke colored white and stroke size increased
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+  background(0);
+  stroke(250);
+  noFill();
 }
 
 function draw() {
-  background(220);
+  if (mouseIsPressed == true){
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    //causes reflection of the line, can be used for a prompt?    (line(width-mouseX, height-mouseY, width-pmouseX, height-pmouseY);)
+  }
+}
+
+function keyTyped(){
+  //saves the canvas
+  if (key == 's'){
+    saveCanvas('fileName', 'png');
+  }
+  //creates an eraser
+  if (key == 'e'){
+    stroke(0);
+    strokeWeight(10);
+  }
+  //uses the "pen" to continue drawing
+  if (key == 'p'){
+    stroke(250);
+    strokeWeight(1);
+  }
 }
