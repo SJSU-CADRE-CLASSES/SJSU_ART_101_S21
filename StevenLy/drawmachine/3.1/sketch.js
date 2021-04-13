2d9706c3132bc4f04bd70a3e2e86d1e0bc370db1
stevebutu <stevenly657@gmai>
2021-04-13 14:13:36 -0700

    3.1

--- StevenLy/drawmachine/3.1/sketch.js
+++ StevenLy/drawmachine/3.1/sketch.js
@@ -10,25 +10,25 @@ function setup() {
 
 function draw() {
 
-if (mouseIsPressed){
-  //line(mouseX, mouseY, pmouseX, pmouseY);
-  array.push([mouseX, mouseY]);
+  if (mouseIsPressed) {
+    //line(mouseX, mouseY, pmouseX, pmouseY);
+    array.push([mouseX, mouseY]);
   }
 
 }
 
-function keyTyped(){
+function keyTyped() {
 
-  if (key == 's'){
+  if (key == 's') {
     // save this image
     saveCanvas('fileName', 'png')
-  } else if (key == 'd'){
+  } else if (key == 'd') {
     //display image
 
 
     beginShape();
-    for(let i = 0; i < array.length; i++){
-    //  line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+    for (let i = 0; i < array.length; i++) {
+      //  line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
       curveVertex(array[i][0], array[i][1])
     }
     endShape();
