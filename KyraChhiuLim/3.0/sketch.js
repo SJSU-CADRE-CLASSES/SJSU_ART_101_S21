d0cc6d13ebf64051ef2197189d15df3851b72181
kyrach-lim <kyrachhiulim@gmail.com>
2021-04-17 18:12:46 -0700

    3.0 again

--- KyraChhiuLim/3.0/sketch.js
+++ KyraChhiuLim/3.0/sketch.js
@@ -22,10 +22,10 @@ function keyTyped(){
   }
 
   for(let i = 0; i < array.length - 1; i++){
-
+    line(array [i][0], array[i][1], array [i + 1][0], array [i + 1][1]);
   }
 
-  line(array [i][0], array[i][1], array [i + 1][0], array [i + 1][1]);
+
 
   return false;
 }
