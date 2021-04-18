4ebe18ae44e604c9f2ffe9ff129f1f77a798206b
Huy_Tran <huytc81@gmail.com>
2021-04-18 02:02:42 -0700

    update 3.2

--- Huy_Tran/randomizer/3.11/sketch.js
+++ Huy_Tran/randomizer/3.11/sketch.js
@@ -1,24 +1,26 @@
 let array = [];
-let backgroundColor = 200;
+let noiseOffset = 0.0;
+let strokeValue = 5;
+// let backgroundColor = 200;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(backgroundColor);
+  background(100, 150, 70, 30);
 
-  strokeWeight(7);
+  strokeWeight(5);
   noFill();
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    // stroke(map(mouseX, 0, 600, 0, 255, true));
-    // line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY, );
-    // line(mouseX, mouseY, pmouseX, pmouseY);
-    backgroundColor -= 7;
-    background(backgroundColor);
-    array.push([mouseX, mouseY]);
+    background(100, 150, 70, 30);
 
-  }
+    strokeWeight(strokeValue);
+    noiseOffset += 0.05;
+    strokeValue = noise(noiseOffset) * 110;
+
+    stroke(map(mouseX, 0, 600, 0, 255, true));
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
 }
 
 function keyTyped() {
@@ -27,15 +29,9 @@ function keyTyped() {
     //save current drawing
     saveCanvas('drawSomething', 'png');
 
-  } else if (key === 'd') {
+  } else if (key === 'c') {
     //display image
-    background(220);
-    beginShape();
-      for (var i = 0; i < array.length; i++) {
-        // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
-        curveVertex(array[i][0], array[i][1]);
-      }
-    endShape();
+  clear();
   }
   return false;
 }
