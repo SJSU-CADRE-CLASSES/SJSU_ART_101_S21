e93c959dd60190a472189effd1feacb2c6103126
Huy_Tran <huytc81@gmail.com>
2021-04-18 03:42:27 -0700

    added 3.3

--- Huy_Tran/randomizer/3.12/sketch.js
+++ Huy_Tran/randomizer/3.12/sketch.js
@@ -1,26 +1,26 @@
 let array = [];
-let noiseOffset = 0.0;
-let strokeValue = 5;
-// let backgroundColor = 200;
+let backgroundColor = 200;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(100, 150, 70, 30);
+  createCanvas(450, 450);
+  // background(backgroundColor);
 
-  strokeWeight(5);
-  noFill();
+
+  drawGrid();
+  // noFill();
 }
 
+
 function draw() {
-    background(100, 150, 70, 30);
+  if (mouseIsPressed) {
+    // stroke(map(mouseX, 0, 600, 0, 255, true));
+    // line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY, );
+    // line(mouseX, mouseY, pmouseX, pmouseY);
+    backgroundColor -= 7;
+    background(backgroundColor);
+    array.push([mouseX, mouseY]);
 
-    strokeWeight(strokeValue);
-    noiseOffset += 0.05;
-    strokeValue = noise(noiseOffset) * 110;
-
-    stroke(map(mouseX, 0, 600, 0, 255, true));
-    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
 }
 
 function keyTyped() {
@@ -29,9 +29,35 @@ function keyTyped() {
     //save current drawing
     saveCanvas('drawSomething', 'png');
 
-  } else if (key === 'c') {
+  } else if (key === 'd') {
     //display image
-  clear();
+    background(220);
+    beginShape();
+      for (var i = 0; i < array.length; i++) {
+        // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+        curveVertex(array[i][0], array[i][1]);
+      }
+    endShape();
   }
   return false;
+}
+
+function drawGrid() {
+  numCells = 20;
+  fillColor = 255;
+  strokeWeight(0);
+
+  for (let i = 0; i <= width; i += width/ numCells) {
+    for (let j = 0; j <= width; j += height/ numCells) {
+      if (fillColor === 255) {
+        fillColor = 200;
+      } else {
+        fillColor = 255;
+      }
+      fill(fillColor);
+      rect(i, j, width/ numCells, height/ numCells);
+    }
+  }
+
+  strokeWeight(10);
 }
