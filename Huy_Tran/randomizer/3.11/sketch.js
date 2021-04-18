e69a51ffdbda8e9e21b5359346237f71b3dd740b
Huy_Tran <huytc81@gmail.com>
2021-04-18 00:43:13 -0700

    Update sketch.js

--- Huy_Tran/randomizer/3.11/sketch.js
+++ Huy_Tran/randomizer/3.11/sketch.js
@@ -31,10 +31,10 @@ function keyTyped() {
     //display image
     background(220);
     beginShape();
-      for (var i = 0; i < array.length; i++) {
-        // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
-        curveVertex(array[i][0], array[i][1]);
-      }
+    for (var i = 0; i < array.length; i++) {
+      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      curveVertex(array[i][0], array[i][1]);
+    }
     endShape();
   }
   return false;
