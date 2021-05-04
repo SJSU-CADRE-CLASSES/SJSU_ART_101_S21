5f3edcbd8113d465e87ff907cf13b0e997649f86
Huy_Tran <huytc81@gmail.com>
2021-05-04 12:05:40 -0700

    update

--- Huy_Tran/randomizer/game_building/sketch.js
+++ Huy_Tran/randomizer/game_building/sketch.js
@@ -1,38 +1,7 @@
-//---------HOW TO BUILD A GAME----------//
-//--------------05/03/2021-------------//
-
-'use strict';
-
-let state = 'title';
-let cnv;
-
 function setup() {
-  createCanvas(450, 600);
-  background(100);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  if (state === 'title') {
-    title();
-  } else if (state === 'level 1') {
-    level1();
-  }
-}
-
-function mousePressed() {
-  state = 'level1';
-}
-
-function title() {
-  background(100);
-  textSize(50);
-  text('VIRUS SHOOTING', 100, 100);
-  stroke(255);
-}
-
-function level1() {
-  background(50 150 200);
-  textSize(50);
-  text('VIRUS SHOOTING', 100, 100);
-  stroke(255);
+  background(220);
 }
