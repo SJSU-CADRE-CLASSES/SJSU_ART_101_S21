a8d0cf046a9e0cedef9b72faf0ecdb3fa99f67b7
Huy_Tran <huytc81@gmail.com>
2021-05-04 22:59:44 -0700

    update

--- Huy_Tran/randomizer/game_building/sketch.js
+++ Huy_Tran/randomizer/game_building/sketch.js
@@ -1,7 +1,36 @@
+//--------------------------NEW DAYS WITH BUILING GAME-------------------------//
+//------------------------------Build 05/03/2021------------------------------//
+'use strict';
+
+let state = 'title';
+let cnv;
+
 function setup() {
-  createCanvas(400, 400);
+  cnv = createCanvas(450, 550);
+  cnv.mouseClicked(function(){
+    console.log('Canvas is Clicked');
+  });
 }
 
 function draw() {
+  if (state === 'title') {
+    title();
+  } else if (state === 'level 1') {
+    level1();
+  }
+}
+
+function title() {
   background(220);
+  text('GERMS', 100, 50);
+  textSize(150);
+  stroke(250);
+
+
+  text('click anywhere else to start', 100, 300);
+  textSize(15);
+}
+
+function level1() {
+  background(50, 150, 250);
 }
