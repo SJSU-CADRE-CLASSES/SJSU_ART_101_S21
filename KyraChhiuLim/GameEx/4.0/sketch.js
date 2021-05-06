75ab34340f1c21644bfc09f283c2689676e1c469
kyrach-lim <kyrachhiulim@gmail.com>
2021-05-06 13:29:46 -0700

    adjust

--- KyraChhiuLim/GameEx/4.0/sketch.js
+++ KyraChhiuLim/GameEx/4.0/sketch.js
@@ -1,7 +1,50 @@
+'uyse strict';
+
+
+let state = 'title';
+let cnv;
+
 function setup() {
-  createCanvas(400, 400);
+  cnv = createCanvas(600, 600);
+
 }
 
 function draw() {
-  background(220);
+
+  switch (state){
+  case 'title':
+    title();
+    cnv.mouseClicked(titleMouseClicked);
+    break;
+  case 'level 1':
+    level1();
+    cnv.mouseClicked(level1MouseClicked);
+    break;
+  default:
+    break;
+
+  }
+}
+
+function title (){
+    background(100);
+    textSize(80);
+    stroke(255);
+    text('MY GAME', 100, 100);
+
+    textSize(30);
+    text('click anywhere to start', 100, 300);
+}
+
+function titleMouseClicked(){
+  console.log('canvas is clicked on title page');
+  state = 'level 1'
+}
+
+function level1(){
+  background(50, 150, 200);
+}
+
+function level1MouseClicked(){
+
 }
