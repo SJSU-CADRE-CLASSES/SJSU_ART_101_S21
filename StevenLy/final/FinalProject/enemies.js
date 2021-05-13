b6d589f16af1ae531f6f5ba2c50400b52a12a670
stevebutu <stevenly657@gmai>
2021-05-13 15:08:11 -0700

    'Final'

---
+++ StevenLy/final/FinalProject/enemies.js
@@ -0,0 +1,16 @@
+class Enemy {
+  constructor(){
+    this.r = 50;
+    this.x = random(w);
+    this.y = 0 - this.r;
+    this.speed = 7;
+  }
+
+  display(){
+    image(enemyImg, this.x, this.y, this.r, this.r);
+  }
+
+  move(){
+    this.y++;
+  }
+}
