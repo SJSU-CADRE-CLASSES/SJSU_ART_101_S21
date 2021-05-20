b70d9242ea2f045fdc7130923fdf77aef4c5ed5d
ahmerahm18 <70781541+ahmerahm18@users.noreply.github.com>
2021-05-19 20:29:56 -0700

    AhmerAhmed

--- AhmerAhmed/Game/4.0/sketch.js
+++ AhmerAhmed/Game/4.0/sketch.js
@@ -1,6 +1,6 @@
 //use remove() to exit the code
 var plvl = 1;
-var php = 95 + (plvl*5);
+var php = 95 + (plvl * 5);
 var pexp = 0;
 var ehp;
 var hpotion;
@@ -11,69 +11,88 @@ var money;
 var enemy;
 var php_pnt;
 var mob;
-var name = 1;
-var counter = 1;
+
+var points = 0;
+
+let state = 'title';
+let cnv;
 
 function setup() {
-  createCanvas(800, 800);
+  cnv = createCanvas(800, 800);
+
   background(0);
   fill(255);
-
-  introScene();
 }
 
 function draw() {
-
-}
-
-function keyTyped(){
-
-}
-
-function keyPressed(){
-    if (keyCode === SHIFT){
-    clear();
-    background(0);
+  switch (state) {
+    case 'title':
+      title();
+      cnv.mouseClicked(titleClick);
+      break;
+    case 'intro':
+      introScene();
+      cnv.mouseClicked(introClick);
+      break;
+    case 'win':
+      winYou();
+      cnv.mouseClicked(winClick);
+    default:
+      break;
   }
 }
 
-function endBattle(){
-
+function keyPressed() {
+  if (keyCode === SHIFT) {
+    clear();
+    background(0);
+    console.log("shift has been clicked")
+  }
 }
 
-function playerName(){
-  console.log(player.value());
+function title() {
+  fill(225);
+  textSize(100);
+  text("This is the TITLE", 10, 300);
+  textSize(50);
+  text("Click anywhere to begin.", 130, 400);
 }
 
-function introScene(){
-  textSize(24);
+function titleClick() {
+  state = 'intro';
+}
+
+function introScene() {
+  clear();
+  background(0);
+  textSize(22);
   fill(255);
   text("Welcome Seaker to the world of Superitor. Your new adventure begins here.", 10, 50);
   text("Please hit 'SHIFT' to continue.", 40, 80);
+  textSize(20);
+  text("Cick to gain points", 40, 110);
 }
 
-function scenes(){
-  if(counter == 1){
-    introScene();
-  }
-  if(counter == 2){
-    clear();
-    background(0);
-    text("new stuff happening", 25, 60);
-  }
-  if(counter == 3){
-    clear();
-    background(0);
-    text("wow this is thingy", 50, 50)
-  }
-  if(counter>=4){
-    clear();
-    background(255);
+function introClick() {
+  state = 'intro';
+  points++
+
+  console.log('points = ' + points);
+
+  if(points >= 10){
+    state = 'win';
   }
 }
 
-function mousePressed(){
-  counter += 1;
-  console.log(counter);
-  scenes();
+function winYou(){
+  clear();
+  background(0);
+  textSize(100);
+  text("YOU HAVE WON!!", 10, 300);
+  text("Click to restart.", 50, 400);
+}
+
+function winClick(){
+  points = 0;
+  state = 'intro';
 }
